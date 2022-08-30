e27b95946ab40747151021431acae433043fba4f
Stuart Roeszler <stuart@roeszler.com>
2022-08-30 12:29:05 +0000

    add rregister to admin page and update migrations

--- app_properties/admin.py
+++ app_properties/admin.py
@@ -1,3 +1,7 @@
+""" Import Modules """
 from django.contrib import admin
 
-# Register your models here.
+from .models import Property, Category
+
+admin.site.register(Property)
+admin.site.register(Category)
