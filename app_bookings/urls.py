0344a137cb526561c802dc59e829ab7808992c96
Stuart Roeszler <stuart@roeszler.com>
2022-09-05 15:53:51 +0000

    add bookings base template to allow modify subsequent .html in app

--- app_bookings/urls.py
+++ app_bookings/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_bookings, name='view_bookings'),
+    path('', views.view_bookings_home, name='bookings_home'),
+    path('choose/', views.view_bookings_stage_1, name='bookings_stage_1'),
 ]
--- app_bookings/views.py
+++ app_bookings/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 
 
-def view_bookings(request):
+def view_bookings_stage_1(request):
     """
     View to render the bookings page
     Looks within the current directory app_bookings/templates/book/book.html
     """
     return render(request, 'book/book.html')
+
+
+def view_bookings_home(request):
+    """
+    View to render the bookings page
+    Looks within the current directory app_bookings/templates/book/book.html
+    """
+    return render(request, 'book/bookings-base.html')
