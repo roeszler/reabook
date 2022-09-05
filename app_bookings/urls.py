051196c901684ca87ef06a19d634504844cc4f59
Stuart Roeszler <stuart@roeszler.com>
2022-09-05 16:37:20 +0000

    add bstart framework for bookings pages

--- app_bookings/urls.py
+++ app_bookings/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.view_bookings_home, name='bookings_home'),
-    path('choose/', views.view_bookings_stage_1, name='bookings_stage_1'),
+    path('booked/', views.view_my_bookings, name='my_bookings'),
+    path('choose/', views.choose_bookings, name='choose_bookings'),
 ]
--- app_bookings/views.py
+++ app_bookings/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 
 
-def view_bookings_stage_1(request):
+def view_my_bookings(request):
     """
     View to render the bookings page
     Looks within the current directory app_bookings/templates/book/book.html
     """
-    return render(request, 'book/book.html')
+    return render(request, 'book/bookings.html')
+
+
+def choose_bookings(request):
+    """
+    View to render the bookings page
+    Looks within the current directory app_bookings/templates/book/book.html
+    """
+    return render(request, 'book/choose-property.html')
 
 
 def view_bookings_home(request):
