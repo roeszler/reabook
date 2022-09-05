6c20f51392e17bd29cdf994ab987201d460318df
Stuart Roeszler <stuart@roeszler.com>
2022-09-05 18:20:27 +0000

    add fade effect for choose properties and table in my inspections list

--- static/js/scrollfade.js
+++ static/js/scrollfade.js
@@ -5,3 +5,13 @@ $(document).ready(function(){
         $("#topnav").css("opacity", 1 - $(window).scrollTop() / ($('#topnav').height() / 0.01));
     });
 });
+
+
+/* Fade in property detail on selection of search results list item */ 
+// $(document).ready(function(){
+//     $('#choice-list a').on('click', function (e) {
+//         e.preventDefault()
+//         $(this).tab('show')
+//     });
+// });
+
