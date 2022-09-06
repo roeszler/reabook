816d7a4112a03c10719c4a7ed30189d0642114f1
Stuart Roeszler <stuart@roeszler.com>
2022-09-06 14:55:36 +0000

    add include remaining bookings-base templates and test links

--- app_bookings/urls.py
+++ app_bookings/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     # path('booked/', views.view_my_bookings, name='my_bookings'),
     path('choose/', views.choose_bookings, name='choose_bookings'),
     path('login/', views.view_bookings_login, name='bookings_login'),
+    path('details/', views.view_booking_details, name='request_details'),
+    path('time/', views.view_booking_select_time, name='select_time'),
+    path('success/', views.view_booking_success, name='booking_success'),
 ]
--- app_bookings/views.py
+++ app_bookings/views.py
@@ -15,12 +15,33 @@ def view_bookings_login(request):
     """
     View to render the bookings login page
     """
-    return render(request, 'book/c-login.html')
+    return render(request, 'book/c-booking-login.html')
+
+
+def view_booking_details(request):
+    """
+    View to render the bookings request details page
+    """
+    return render(request, 'book/d-booking-details.html')
+
+
+def view_booking_select_time(request):
+    """
+    View to render the bookings select available time page
+    """
+    return render(request, 'book/e-booking-select-time.html')
+
+
+def view_booking_success(request):
+    """
+    View to render the bookings successful booking page
+    """
+    return render(request, 'book/f-booking-success.html')
 
 
 def choose_bookings(request):
     """
-    View to render the bookings page
+    View to render the choose bookings page
     Looks within the current directory app_bookings/templates/book/book.html
     """
     properties = Property.objects.all()
@@ -40,29 +61,29 @@ def choose_bookings(request):
         'prop4': prop4,
         'prop5': prop5,
     }
-    return render(request, 'book/b-choose-property.html', context)
+    return render(request, 'book/b-booking-choose-property.html', context)
 
 
-def view_bookings_home(request):
-    """
-    View to render the bookings page
-    Looks within the current directory app_bookings/templates/book/book.html
-    """
-    properties = Property.objects.all()
+# def view_bookings_home(request):
+#     """
+#     View to render the bookings home page
+#     Looks within the current directory app_bookings/templates/book/book.html
+#     """
+#     properties = Property.objects.all()
 
-    # Retrieves a single instance of a property in the DB
-    prop1 = Property.objects.get(pk=1)
-    prop2 = Property.objects.get(pk=2)
-    prop3 = Property.objects.get(pk=3)
-    prop4 = Property.objects.get(pk=4)
-    prop5 = Property.objects.get(pk=5)
+#     # Retrieves a single instance of a property in the DB
+#     prop1 = Property.objects.get(pk=1)
+#     prop2 = Property.objects.get(pk=2)
+#     prop3 = Property.objects.get(pk=3)
+#     prop4 = Property.objects.get(pk=4)
+#     prop5 = Property.objects.get(pk=5)
 
-    context = {
-        'properties': properties,
-        'prop1': prop1,
-        'prop2': prop2,
-        'prop3': prop3,
-        'prop4': prop4,
-        'prop5': prop5,
-    }
-    return render(request, 'book/a-bookings.html', context)
+#     context = {
+#         'properties': properties,
+#         'prop1': prop1,
+#         'prop2': prop2,
+#         'prop3': prop3,
+#         'prop4': prop4,
+#         'prop5': prop5,
+#     }
+#     return render(request, 'book/a-bookings.html', context)
