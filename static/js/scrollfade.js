bbd25ccf4a50951a45991e8bb8e8aa06b3686618
Stuart Roeszler <stuart@roeszler.com>
2022-09-07 17:08:48 +0000

    add subnav heading clarity

--- static/js/scrollfade.js
+++ static/js/scrollfade.js
@@ -1,7 +1,8 @@
 /* Fade #main-nav on scroll */
 $(document).ready(function(){
     $(window).scroll(function(){
-        $("#main-nav").css("opacity", 1 - $(window).scrollTop() / ($('#main-nav').height() / 2));
+        $("#sub-nav").css("opacity", 1 - $(window).scrollTop() / ($('#sub-nav').height() / 2));
+        $("#sub-nav-dark").css("opacity", 1 - $(window).scrollTop() / ($('#sub-nav-dark').height() / 2));
         $("#topnav").css("opacity", 1 - $(window).scrollTop() / ($('#topnav').height() / 0.01));
     });
 });
