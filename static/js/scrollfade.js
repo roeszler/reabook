95f37d2c85711843210d104d1c6817e325b891c9
Stuart Roeszler <stuart@roeszler.com>
2022-09-07 18:44:19 +0000

    add styling and auto-dropdown js to dropdown navs

--- static/js/scrollfade.js
+++ static/js/scrollfade.js
@@ -2,9 +2,22 @@
 $(document).ready(function(){
     $(window).scroll(function(){
         $("#sub-nav").css("opacity", 1 - $(window).scrollTop() / ($('#sub-nav').height() / 2));
-        $("#sub-nav-dark").css("opacity", 1 - $(window).scrollTop() / ($('#sub-nav-dark').height() / 2));
         $("#topnav").css("opacity", 1 - $(window).scrollTop() / ($('#topnav').height() / 0.01));
     });
+
+    $('.dropdown').hover(function(){
+        $('.auto-dropdown', this).trigger('click');
+    });
+
+    $('#choice-list a').on('click', function (e) {
+        e.preventDefault()
+        $(this).tab('show')
+    });
+
+    // $('.auto-dropdown').onload(function(){
+    //     // this.style.opacity = "100%";
+    //     this.style.span.hover.transition = "1.3s";
+    // });
 });
 
 
