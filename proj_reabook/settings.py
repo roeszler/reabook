a4fc52d90887986c854a566f0791d22567ce0ea1
Stuart Roeszler <stuart@roeszler.com>
2022-09-08 09:54:33 +0000

    rebase from error in image upload

--- proj_reabook/settings.py
+++ proj_reabook/settings.py
@@ -166,12 +166,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Connect cloudinary to static files:
-# STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-# DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
