8ec111eb981c2a9835a53540daed9a59dbd95424
Stuart Roeszler <stuart@roeszler.com>
2022-09-08 06:07:52 +0000

    add .js reference to base.html accross site

--- static/js/scrollfade.js
+++ static/js/scrollfade.js
@@ -1,31 +1,19 @@
-/* Fade #main-nav on scroll */
+/* Custom js */
 $(document).ready(function(){
+    // Fade #sub-nav on scroll
     $(window).scroll(function(){
         $("#sub-nav").css("opacity", 1 - $(window).scrollTop() / ($('#sub-nav').height() / 2));
         $("#topnav").css("opacity", 1 - $(window).scrollTop() / ($('#topnav').height() / 0.01));
     });
 
+    // Dropdown list display on hover
     $('.dropdown').hover(function(){
         $('.auto-dropdown', this).trigger('click');
     });
 
+    // Control property choice list on book/choose/ booking-choose-property.html
     $('#choice-list a').on('click', function (e) {
         e.preventDefault()
         $(this).tab('show')
     });
-
-    // $('.auto-dropdown').onload(function(){
-    //     // this.style.opacity = "100%";
-    //     this.style.span.hover.transition = "1.3s";
-    // });
 });
-
-
-/* Fade in property detail on selection of search results list item */ 
-// $(document).ready(function(){
-//     $('#choice-list a').on('click', function (e) {
-//         e.preventDefault()
-//         $(this).tab('show')
-//     });
-// });
-
