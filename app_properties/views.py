dd7ea3bafc3419344e45aac3cf5abf7ec5aad431
Stuart Roeszler <stuart@roeszler.com>
2022-09-09 06:56:41 +0000

    add search functions

--- app_properties/views.py
+++ app_properties/views.py
@@ -1,5 +1,7 @@
 """ Import Modules """
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
 
 from .models import Property
 
@@ -9,8 +11,26 @@ def all_properties(request):
     View to render all properties page that includes search queries and sorting
     """
     properties = Property.objects.all()
+    query = None
+
+    # search function
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(
+                    request, "You didn't enter any search criteria!")
+                return redirect(reverse('properties'))
+
+            # Using Q to generate a non-sensitive search query
+            # by name OR description
+            queries = Q(name__icontains=query) | Q(description__icontains=query) | Q(ribbon_feature__icontains=query) | Q(sale_price__icontains=query) | Q(rent_pw__icontains=query) | Q(suburb__icontains=query) | Q(street__icontains=query) | Q(city__icontains=query) | Q(state__icontains=query) | Q(country__icontains=query) | Q(postcode__icontains=query)
+
+            properties = properties.filter(queries)
+
     context = {
         'properties': properties,
+        'search_term': query,
     }
     return render(request, 'properties/properties.html', context)
 
