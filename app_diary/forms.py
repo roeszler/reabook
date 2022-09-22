35a7a4c024b46ee5d7cf496beea972173d2f8e66
Stuart Roeszler <stuart@roeszler.com>
2022-09-22 13:24:21 +0000

    add custom form in register_user view

---
+++ app_diary/forms.py
@@ -0,0 +1,17 @@
+""" Import Models """
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+
+class RegisterUserFrom(UserCreationForm):
+    email = forms.EmailField()
+    first_name = forms.CharField(max_length=75)
+    last_name = forms.CharField(max_length=75)
+
+    class Meta:
+        model = User
+        fields = (
+            'username', 'first_name', 'last_name',
+            'email', 'password1', 'password2'
+        )
