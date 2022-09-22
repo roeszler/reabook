f5fc7ab42ffb73bcb47b037e061b7916d3875104
Stuart Roeszler <stuart@roeszler.com>
2022-09-22 09:13:26 +0000

    add alert message on login incorrect

--- app_diary/views.py
+++ app_diary/views.py
@@ -15,7 +15,20 @@ from django.contrib import messages
 
 def login_user(request):
     """ To render the bookings page with clients upcoming appointments """
-    return render(request, 'diary/login.html', {})
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page.
+            return redirect('client_diary')
+        else:
+            messages.success(request, 'Aww Nuts! There was an error Logging you in. Please try again...')
+            # Return an 'invalid login' error message.
+            return redirect('login')
+    else:
+        return render(request, 'diary/login.html', {})
 
 
 def register_user(request):
