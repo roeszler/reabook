8d95e0bfbd583e46f0cf306930476b30f45d046e
Stuart Roeszler <stuart@roeszler.com>
2022-09-25 15:58:02 +0000

    add update to app_property  models

--- app_properties/forms.py
+++ app_properties/forms.py
@@ -4,7 +4,7 @@ from .models import Property
 
 
 class PropertyForm(forms.ModelForm):
-    """ To customize the django form to a Bookings form """
+    """ To allow staff / agents to read and update properties in the db """
     # category = forms.CharField(max_length=75)
     class Meta:
         model = Property
@@ -54,7 +54,6 @@ class PropertyForm(forms.ModelForm):
         widgets = {
             'category': forms.Select(attrs={'class':'form-control col-12', }),
             'sector': forms.Select(attrs={'class':'form-control col-lg-12', }),
-            # 'title_no': forms.CharField(attrs={'class':'form-control col-lg-12', }),
             'title_no': forms.TextInput(attrs={'class':'form-control col-lg-12', }),
             'ribbon_feature': forms.TextInput(attrs={'class':'form-control col-lg-12', }),
             'name': forms.TextInput(attrs={'class':'form-control col-lg-12', }),
@@ -90,19 +89,4 @@ class PropertyForm(forms.ModelForm):
             'owner_fname': forms.TextInput(attrs={'class':'form-control col-lg-12', }),
             'owner_lname': forms.TextInput(attrs={'class':'form-control col-lg-12', }),
             'realtor': forms.TextInput(attrs={'class':'form-control col-lg-12', }),
-            # 'viewings': forms.CheckboxInput(attrs={'class':'form-control col-lg-12', }),
-        #     'client_password': forms.TextInput(attrs={'class':'form-control col-lg-5', 'placeholder':'can't be blank', }),
         }
-
-        # labels = {
-        #     'f_name': 'First Name',
-        #     'l_name': 'Last Name',
-        #     'client_username': 'Username',
-        #     'client_email': 'Email Address',
-        #     'client_phone': 'Phone Number',
-        #     'date_of_viewing': 'Date of Viewing Appointment',
-        #     'time_of_viewing': 'Time of Appointment',
-        #     'client_message': 'Messages: ',
-        #     'contact_ok': 'Permission to refer for other Properties',
-        #     # 'client_password': 'Password',
-        # }
