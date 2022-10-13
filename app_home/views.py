7810d63dbeaff5f9d838416c88b0655e704e16db
Stuart Roeszler <stuart@roeszler.com>
2022-10-13 18:26:25 +0000

    debug STATICFILES_DIRS deploy to heroku

--- app_home/views.py
+++ app_home/views.py
@@ -2,15 +2,20 @@
 from django.shortcuts import render
 
 from app_properties.models import Property
+from app_bookings.models import Booking
 
 
 def index(request):
     """ View to return the index.html page """
-    properties = Property.objects.all()
+    properties = Property.objects.all() # noqa
     props_latest = properties.order_by('list_date')
     props_for_rent = properties.filter(category__name="for_rent")
     props_for_sale = properties.filter(category__name="for_sale")
     props_for_lease = properties.filter(category__name="for_lease")
+
+    user = request.user
+    users_bookings = Booking.objects.filter(user=user) # noqa
+    bookings_count = users_bookings.count()
 
     context = {
         'properties': properties,
@@ -18,5 +23,6 @@ def index(request):
         'props_for_rent': props_for_rent,
         'props_for_sale': props_for_sale,
         'props_for_lease': props_for_lease,
+        'bookings_count': bookings_count,
     }
     return render(request, 'home/index.html', context)
--- proj_reabook/views.py
+++ proj_reabook/views.py
@@ -1,22 +1,7 @@
 """ Import Modules """
 from django.shortcuts import render
-from django.contrib.auth.models import User
-
-from app_bookings.models import Booking
 
 
 def handler404(request, exception):
     """ Error Handler 404 - Page Not Found """
-    return render(request, "errors/404.html", status=404)
-
-
-def bookings_count(request):
-    """ Return the total number of bookings for the logged in user """
-    user = request.user
-    users_bookings = Booking.objects.filter(user=user) # noqa
-    no_bookings = users_bookings.count()
-
-    context = {
-        'no_bookings': no_bookings,
-    }
-    return render(request, 'base.html', context)
+    return render(request, "/errors/404.html", status=404)
